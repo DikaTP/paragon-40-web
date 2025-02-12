8dcb1944c90eea2e7e6b471cae46794ae413a6ef
Andhika TP <tpandhika@gmail.com>
2025-02-12 12:57:15 +0700

    fix locale

--- src/services/locale.js
+++ src/services/locale.js
@@ -3,14 +3,14 @@
 import { cookies } from 'next/headers';
 import { defaultLocale } from '@/i18n/config';
 
-// In this example the locale is read from a cookie. You could alternatively
-// also read it from a database, backend service, or any other source.
 const COOKIE_NAME = 'NEXT_LOCALE';
 
 export async function getUserLocale() {
-  return cookies().get(COOKIE_NAME)?.value || defaultLocale;
+  const cookieStore = await cookies(); // Await the cookies() function
+  return cookieStore.get(COOKIE_NAME)?.value || defaultLocale;
 }
 
 export async function setUserLocale(locale) {
-  cookies().set(COOKIE_NAME, locale);
+  const cookieStore = await cookies(); // Await the cookies() function
+  cookieStore.set(COOKIE_NAME, locale);
 }
