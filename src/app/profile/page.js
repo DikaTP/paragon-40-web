3bed1e45653a801555c5f01d9d4083b7730d706b
michael leonardo <leonardomichael99@gmail.com>
2025-02-13 23:26:58 +0700

    removed duplicate profile page

--- src/app/profile/page.js
+++
@@ -1,8 +0,0 @@
-import {useLocale, useTranslations} from 'next-intl';
-export default function ProfilePage() {
-  const t = useTranslations('ProfilePage');
-  
-  return (
-    <div>{t('title')}</div>
-  );
-}
