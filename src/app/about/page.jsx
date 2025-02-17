7c0ffd2dd33b374ecd6c636eefbce019f6e7076c
Andhika TP <tpandhika@gmail.com>
2025-02-17 14:05:45 +0700

    update about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -21,11 +21,13 @@ export default function AboutPage() {
           </div>
         </div>
         <div className="grid grid-cols-4 gap-4">
-          <Image className="rounded-3xl w-full h-full col-span-4 lg:col-span-1" src="/dream_sm.png" alt="dream" width="300" height="500" />
-
+          {/* <Image className="rounded-3xl w-full h-full col-span-4 lg:col-span-1" src="/dream_sm.png" alt="dream" width="300" height="500" /> */}
+          <video controls className='rounded-3xl w-full h-full col-span-4 lg:col-span-1'>
+            <source src="https://storage.googleapis.com/paragon-p40-cdn/movie/FINAL%20CAMPAIGN%20P40.mp4" type="video/mp4"/>
+          </video>
           <div className="bg-kv-gradient col-span-4 lg:col-span-3 rounded-3xl">
             <div className="p-6  text-white">
-              <h1 className="text-4xl lg:text-6xl font-bold mb-10">UNLOCK<br />NEW THINKING</h1>
+              <h1 className="text-4xl lg:text-6xl font-bold mb-10">UNLOCKING<br />NEW THINKING</h1>
               <p className="text-base mb-5">{t('AboutPage.description1')}</p>
               <p className="text-base mb-5">{t("AboutPage.description2")}</p>
               <p className="text-base">{t("AboutPage.description3")}</p>
